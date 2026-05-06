db48b0a2b054c11c1cd797c32d9b602c085d26ce
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-06 14:14:26 +0100

    feat: auth service entity and repository

---
+++ auth-service/src/main/java/com/nabgha/auth_service/entity/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.nabgha.auth_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "refresh_tokens")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Long;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiryDate;
+
+    @Column(name = "last_used_at")
+    private LocalDateTime lastUsedAt;
+
+    public boolean isExpired(){
+        return LocalDateTime.now().isAfter(expiryDate);
+    }
+}
